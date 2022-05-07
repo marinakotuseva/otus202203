2efbc4a5a2a8bac8c153f5240841257538098123
Marina <34891005+marinakotuseva@users.noreply.github.com>
2022-05-07 17:23:29 +0300

    Hw02 generics (#2)
    [HW02][ Generics]

---
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -0,0 +1,34 @@
+package homework;
+
+
+import java.util.*;
+
+public class CustomerService {
+    private final Map<Customer, String> customers = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+
+    public Map.Entry<Customer, String> getSmallest() {
+        Optional<Map.Entry<Customer, String>> smallestOpt = customers.entrySet().stream().findFirst()
+            .map(entry -> new AbstractMap.SimpleImmutableEntry<>(entry.getKey(), entry.getValue()));
+
+        if (smallestOpt.isEmpty()) return null;
+
+        Map.Entry<Customer, String> currentData = smallestOpt.get();
+        return new AbstractMap.SimpleEntry<>(getNewCustomer(currentData.getKey()), currentData.getValue());
+    }
+
+    private Customer getNewCustomer(Customer currentCustomer) {
+        return new Customer(currentCustomer.getId(), currentCustomer.getName(), currentCustomer.getScores());
+    }
+
+    public Map.Entry<Customer, String> getNext(Customer customer) {
+        return customers.entrySet().stream()
+            .filter(entry -> entry.getKey().getScores() > customer.getScores())
+            .findFirst()
+            .map(entry -> new AbstractMap.SimpleEntry<>(getNewCustomer(entry.getKey()), entry.getValue()))
+            .orElse(null);
+    }
+
+    public void add(Customer customer, String data) {
+        customers.put(customer, data);
+    }
+}
