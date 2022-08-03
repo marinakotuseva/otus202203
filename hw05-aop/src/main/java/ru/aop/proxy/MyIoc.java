b892003e111c5dcca23ae42b93e67a065e5ece96
Marina Kotuseva <kotuseva@tochka.com>
2022-08-03 19:13:50 +0300

    [HW05][ Add logging]

---
+++ hw05-aop/src/main/java/ru/aop/proxy/MyIoc.java
@@ -0,0 +1,20 @@
+package ru.aop.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+import ru.aop.model.MyClass;
+import ru.aop.model.MyInterface;
+
+/**
+ * @author Marina Kotuseva
+ */
+public class MyIoc {
+
+    static MyInterface createMyInterface() {
+        InvocationHandler handler = new MyInvocationHandler(new MyClass());
+        return (MyInterface) Proxy.newProxyInstance(MyIoc.class.getClassLoader(),
+                new Class<?>[]{MyInterface.class}, handler);
+    }
+
+}
